4ff8102f4c9b969cccd959193dfae8090a5c6b97
Debasishbsws <debasishbsws.abc@gmail.com>
2022-05-28 12:13:50 +0530

    look 10% done

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index", {});
-})
+});
+
+// app.get("/blogs", (req, res) => {
+//     res.render("blogHome", {});
+// });
 
 
 
